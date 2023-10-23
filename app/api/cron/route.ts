8bbc60dc0903a8cdc6c7ce98cf8901f1af8d7861
erichli1 <ehli@college.harvard.edu>
2023-10-22 23:48:52 -0400

    Remove logic of non-functional routes to prevent Prisma errors during deployment

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,90 +1,102 @@
-import { NextResponse } from "next/server"
-import axios, { AxiosError } from "axios"
-import { load } from "cheerio"
-import { PrismaClient } from "@prisma/client"
+import { NextRequest, NextResponse } from "next/server"
 
-const prisma = new PrismaClient()
+// import axios, { AxiosError } from "axios"
+// import { load } from "cheerio"
+// import { PrismaClient } from "@prisma/client"
 
-export async function GET() {
-    const date = new Date()
-    date.setDate(date.getDate() + 6)
+// const prisma = new PrismaClient()
 
-    const lunchResults = await fetchRowsInTable({
-        date: `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`,
-        lunch: true,
-    })
-    const lunchEntrees = pullEntreesFromRows(lunchResults)
+// export async function GET() {
+//     const date = new Date()
+//     date.setDate(date.getDate() + 6)
 
-    const dinnerResults = await fetchRowsInTable({
-        date: `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`,
-        lunch: false,
-    })
-    const dinnerEntrees = pullEntreesFromRows(dinnerResults)
+//     const lunchResults = await fetchRowsInTable({
+//         date: `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`,
+//         lunch: true,
+//     })
+//     const lunchEntrees = pullEntreesFromRows(lunchResults)
 
-    await addEntriesToDatabase({ entrees: lunchEntrees, date, lunch: true })
-    await addEntriesToDatabase({ entrees: dinnerEntrees, date, lunch: false })
+//     const dinnerResults = await fetchRowsInTable({
+//         date: `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`,
+//         lunch: false,
+//     })
+//     const dinnerEntrees = pullEntreesFromRows(dinnerResults)
 
-    // TODO: add error handling
+//     await addEntriesToDatabase({ entrees: lunchEntrees, date, lunch: true })
+//     await addEntriesToDatabase({ entrees: dinnerEntrees, date, lunch: false })
+
+//     // TODO: add error handling
+//     return NextResponse.json(
+//         {
+//             body: {
+//                 lunch: lunchEntrees,
+//                 dinner: dinnerEntrees,
+//                 date,
+//             },
+//         },
+//         {
+//             status: 200,
+//         },
+//     )
+// }
+
+// function fetchRowsInTable({ date, lunch }: { date: string; lunch: boolean }): Promise<Array<string>> {
+//     const meal = lunch ? "1" : "2"
+
+//     return axios
+//         .get(`https://www.foodpro.huds.harvard.edu/foodpro/menu_items.asp?date=${date}&type=05&meal=${meal}`)
+//         .then((res) => {
+//             const html = res.data
+//             const $ = load(html)
+
+//             const rows: Array<string> = []
+
+//             const trElements = $("tr")
+//             trElements.each((_, element) => {
+//                 const rowData = $(element)
+//                     .text()
+//                     .replace(/[\t\n]/g, "") // Remove tabs and newlines
+//                     .split(/(\d|\|)/)[0] // Only keep substring up to first number or pipe character
+//                     .trim()
+//                 rows.push(rowData)
+//             })
+
+//             return rows
+//         })
+//         .catch((error: AxiosError) => {
+//             console.error(`There was an error with ${error.config?.url}.`)
+//             console.error(error.toJSON())
+
+//             return []
+//         })
+// }
+
+// function pullEntreesFromRows(rows: Array<string>): Array<string> {
+//     const entreesIndex = rows.indexOf("Entrees")
+//     const vegVeganIndex = rows.indexOf("Veg,Vegan")
+
+//     return rows.slice(entreesIndex + 1, vegVeganIndex)
+// }
+
+// async function addEntriesToDatabase({ entrees, date, lunch }: { entrees: Array<string>; date: Date; lunch: boolean }) {
+//     await entrees.forEach(async (entree) => {
+//         await prisma.menu.create({
+//             data: {
+//                 name: entree,
+//                 date,
+//                 lunch,
+//             },
+//         })
+//     })
+// }
+
+export async function GET(request: NextRequest) {
     return NextResponse.json(
         {
-            body: {
-                lunch: lunchEntrees,
-                dinner: dinnerEntrees,
-                date,
-            },
+            body: "currently non-functional",
         },
         {
             status: 200,
         },
     )
-}
-
-function fetchRowsInTable({ date, lunch }: { date: string; lunch: boolean }): Promise<Array<string>> {
-    const meal = lunch ? "1" : "2"
-
-    return axios
-        .get(`https://www.foodpro.huds.harvard.edu/foodpro/menu_items.asp?date=${date}&type=05&meal=${meal}`)
-        .then((res) => {
-            const html = res.data
-            const $ = load(html)
-
-            const rows: Array<string> = []
-
-            const trElements = $("tr")
-            trElements.each((_, element) => {
-                const rowData = $(element)
-                    .text()
-                    .replace(/[\t\n]/g, "") // Remove tabs and newlines
-                    .split(/(\d|\|)/)[0] // Only keep substring up to first number or pipe character
-                    .trim()
-                rows.push(rowData)
-            })
-
-            return rows
-        })
-        .catch((error: AxiosError) => {
-            console.error(`There was an error with ${error.config?.url}.`)
-            console.error(error.toJSON())
-
-            return []
-        })
-}
-
-function pullEntreesFromRows(rows: Array<string>): Array<string> {
-    const entreesIndex = rows.indexOf("Entrees")
-    const vegVeganIndex = rows.indexOf("Veg,Vegan")
-
-    return rows.slice(entreesIndex + 1, vegVeganIndex)
-}
-
-async function addEntriesToDatabase({ entrees, date, lunch }: { entrees: Array<string>; date: Date; lunch: boolean }) {
-    await entrees.forEach(async (entree) => {
-        await prisma.menu.create({
-            data: {
-                name: entree,
-                date,
-                lunch,
-            },
-        })
-    })
 }
--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,32 +1,43 @@
-import { NextResponse } from "next/server"
-import type { NextRequest } from "next/server"
-import { PrismaClient } from "@prisma/client"
+import { NextRequest, NextResponse } from "next/server"
 
-const prisma = new PrismaClient()
+// import { PrismaClient } from "@prisma/client"
+
+// const prisma = new PrismaClient()
+
+// export async function GET(request: NextRequest) {
+//     const tests = await getTests()
+
+//     return NextResponse.json(
+//         {
+//             body: tests,
+//         },
+//         {
+//             status: 200,
+//         },
+//     )
+// }
+
+// async function getTests() {
+//     return prisma.test.findMany()
+// }
+
+// getTests()
+//     .then(async () => {
+//         await prisma.$disconnect()
+//     })
+//     .catch(async (e) => {
+//         console.error(e)
+//         await prisma.$disconnect()
+//         process.exit(1)
+//     })
 
 export async function GET(request: NextRequest) {
-    const tests = await getTests()
-
     return NextResponse.json(
         {
-            body: tests,
+            body: "currently non-functional",
         },
         {
             status: 200,
         },
     )
 }
-
-async function getTests() {
-    return prisma.test.findMany()
-}
-
-getTests()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
