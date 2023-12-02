3819a3cf8149cfa9bfc235c6b129995119103928
erichli1 <ehli@college.harvard.edu>
2023-12-02 01:23:37 -0500

    Remove unnecessary leftover files

--- prisma/migrations/0_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "test" (
-    "id" VARCHAR(255) NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "test_id_key" ON "test"("id");
-
--- prisma/migrations/20231022200724_add_menu_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Menu" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "date" TIMESTAMP(3) NOT NULL,
-    "active" BOOLEAN NOT NULL,
-
-    CONSTRAINT "Menu_pkey" PRIMARY KEY ("id")
-);
